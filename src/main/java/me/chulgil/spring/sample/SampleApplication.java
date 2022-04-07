1012afe73f6dadf02c8904814080c973e6553007
chulgil <contact@chulgil.me>
2022-04-07 15:23:25 +0900

    Feat: 로그 추적기 추가

---
+++ src/main/java/me/chulgil/spring/sample/SampleApplication.java
@@ -0,0 +1,14 @@
+package me.chulgil.spring.sample;
+
+import me.chulgil.spring.aop.AopApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplication.class, args);
+    }
+
+}
---
+++ src/main/java/me/chulgil/spring/sample/trace/hellotrace/HelloTraceV1.java
@@ -0,0 +1,64 @@
+package me.chulgil.spring.sample.trace.hellotrace;
+
+import lombok.extern.slf4j.Slf4j;
+import me.chulgil.spring.sample.trace.TraceId;
+import me.chulgil.spring.sample.trace.TraceStatus;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class HelloTraceV1 {
+
+    private static final String START_PREFIX = "-->";
+    private static final String COMPLETE_PREFIX = "<--";
+    private static final String EX_PREFIX = "<X-";
+
+    /**
+     * 로그 시작 : 로그 메시지를 파라미터로 받아서 로그를 출력
+     * @param message
+     * @return TraceStatus
+     */
+    public TraceStatus begin(String message) {
+        TraceId traceId = new TraceId();
+        Long startTimeMs = System.currentTimeMillis();
+        log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX, traceId.getLevel()), message);
+        return new TraceStatus(traceId, startTimeMs, message);
+    }
+
+    /**
+     * 로그 종료 : 상태 값으로 실행 시간을 계산 후 종료시 출력
+     * @param status
+     */
+    public void end(TraceStatus status) {
+        complete(status, null);
+    }
+
+    /**
+     * 예외 종료 : 로그를 예외 상황 으로 종료
+     * @param status
+     * @param e
+     */
+    public void exception(TraceStatus status, Exception e) {
+        complete(status, e);
+    }
+
+    private void complete(TraceStatus status, Exception e) {
+        Long stopTimeMs = System.currentTimeMillis();
+        long resultTimeMs = stopTimeMs - status.getStartTimeMs();
+        TraceId traceId = status.getTraceId();
+        if (e == null) {
+            log.info("[{}] {}{} time={}ms", traceId.getId(), addSpace(COMPLETE_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs);
+        } else {
+            log.info("[{}] {}{} time={}ms ex={}", traceId.getId(), addSpace(EX_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs, e.toString());
+        }
+    }
+
+    private static String addSpace(String prefix, int level) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < level; i++) {
+            sb.append((i == level - 1) ? "|" + prefix : "|   ");
+        }
+        return sb.toString();
+    }
+
+}
