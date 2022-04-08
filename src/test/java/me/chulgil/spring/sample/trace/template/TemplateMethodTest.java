5a24a81cc839f398345d6237143bd27d5aac17f5
chulgil <contact@chulgil.me>
2022-04-08 15:59:03 +0900

    Test: 템플릿 메서드 패턴 다형성

--- src/test/java/me/chulgil/spring/sample/trace/template/TemplateMethodTest.java
+++ src/test/java/me/chulgil/spring/sample/trace/template/TemplateMethodTest.java
@@ -1,6 +1,9 @@
 package me.chulgil.spring.sample.trace.template;
 
 import lombok.extern.slf4j.Slf4j;
+import me.chulgil.spring.sample.trace.template.code.AbstractTemplate;
+import me.chulgil.spring.sample.trace.template.code.SubClassLogic1;
+import me.chulgil.spring.sample.trace.template.code.SubClassLogic2;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
@@ -27,4 +30,17 @@ public class TemplateMethodTest {
         long resultTime = endTime - startTime;
         log.info("resultTime={}", resultTime);
     }
+
+    /**
+     * 템플릿 메서드 패턴 적용
+     */
+    @Test
+    void templateMethodV1() {
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
+    }
+
+
 }
