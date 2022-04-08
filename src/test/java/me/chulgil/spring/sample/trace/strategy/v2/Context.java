8686242bcc84d8ac76064db34e8b5abef77ff57b
chulgil <contact@chulgil.me>
2022-04-08 19:52:53 +0900

    Test: 전략 패턴 완성

--- src/test/java/me/chulgil/spring/sample/trace/strategy/v2/Context.java
+++ src/test/java/me/chulgil/spring/sample/trace/strategy/v2/Context.java
@@ -7,14 +7,7 @@ import me.chulgil.spring.sample.trace.strategy.Strategy;
 @Slf4j
 public class Context {
 
-    private final Strategy strategy;
-
-    public Context(Strategy strategy) {
-        this.strategy = strategy;
-    }
-
-
-    public void execute() {
+    public void execute(Strategy strategy) {
         long startTime = System.currentTimeMillis();
         //비즈니스 로직 실행
         strategy.call(); //위임
--- src/test/java/me/chulgil/spring/sample/trace/strategy/v2/ContextTest.java
+++ src/test/java/me/chulgil/spring/sample/trace/strategy/v2/ContextTest.java
@@ -9,104 +9,44 @@ import org.junit.jupiter.api.Test;
 @Slf4j
 public class ContextTest {
 
-    @Test
-    void strategyV0() {
-        logic1();
-        logic2();
-    }
-
     /**
      * 전략 패턴 적용
      */
     @Test
     void strategyV1() {
-        Strategy strategyLogic1 = new StrategyLogic1();
-        Context context1 = new Context(strategyLogic1);
-        context1.execute();
-        Strategy strategyLogic2 = new StrategyLogic2();
-        Context context2 = new Context(strategyLogic2);
-        context2.execute();
+        Context context = new Context();
+        context.execute(new StrategyLogic1());
+        context.execute(new StrategyLogic2());
     }
-
 
     /**
      * 전략 패턴 익명 내부 클래스
      */
     @Test
     void strategyV2() {
-
-        Strategy strategyLogic1 = new Strategy() {
+        Context context = new Context();
+        context.execute(new Strategy() {
             @Override
             public void call() {
                 log.info("비즈니스 로직1 실행");
             }
-        };
-        log.info("strategyLogic1={}", strategyLogic1.getClass());
-        Context context1 = new Context(strategyLogic1);
-        context1.execute();
-
-        Strategy strategyLogic2 = new Strategy() {
+        });
+        context.execute(new Strategy() {
             @Override
             public void call() {
                 log.info("비즈니스 로직2 실행");
             }
-        };
-        log.info("strategyLogic2={}", strategyLogic2.getClass());
-        Context context2 = new Context(strategyLogic2);
-        context2.execute();
+        });
     }
 
     /**
-     * 전략 패턴 익명 내부 클래스2
+     * 전략 패턴 익명 내부 클래스2,람다
      */
     @Test
     void strategyV3() {
-        Context context1 = new Context(new Strategy() {
-            @Override
-            public void call() {
-                log.info("비즈니스 로직1 실행");
-            }
-        });
-        context1.execute();
-
-        Context context2 = new Context(new Strategy() {
-            @Override
-            public void call() {
-                log.info("비즈니스 로직2 실행");
-            }
-        });
-        context2.execute();
+        Context context = new Context();
+        context.execute(() -> log.info("비즈니스 로직1 실행"));
+        context.execute(() -> log.info("비즈니스 로직2 실행"));
     }
 
-    /**
-     * 전략 패턴, 람다
-     */
-    @Test
-    void strategyV4() {
-        Context context1 = new Context(() -> log.info("비즈니스 로직1 실행"));
-        context1.execute();
-
-        Context context2 = new Context(() -> log.info("비즈니스 로직2 실행"));
-        context2.execute();
-    }
-
-    private void logic1() {
-        long startTime = System.currentTimeMillis();
-        //비즈니스 로직 실행
-        log.info("비즈니스 로직1 실행");
-        //비즈니스 로직 종료
-        long endTime = System.currentTimeMillis();
-        long resultTime = endTime - startTime;
-        log.info("resultTime={}", resultTime);
-    }
-
-    private void logic2() {
-        long startTime = System.currentTimeMillis();
-        //비즈니스 로직 실행
-        log.info("비즈니스 로직2 실행");
-        //비즈니스 로직 종료
-        long endTime = System.currentTimeMillis();
-        long resultTime = endTime - startTime;
-        log.info("resultTime={}", resultTime);
-    }
 }
