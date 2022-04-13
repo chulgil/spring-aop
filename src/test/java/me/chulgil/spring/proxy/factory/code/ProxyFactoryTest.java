ab203bc5eb2db6d898dc6f4f70d45c9cebaaa13d
chulgil <contact@chulgil.me>
2022-04-13 12:22:43 +0900

    Test: 스프링이 제공하는 ProxyFactory 테스트

---
+++ src/test/java/me/chulgil/spring/proxy/factory/code/ProxyFactoryTest.java
@@ -0,0 +1,79 @@
+package me.chulgil.spring.proxy.factory.code;
+
+import lombok.extern.slf4j.Slf4j;
+import me.chulgil.spring.proxy.common.advice.TimeAdvice;
+import me.chulgil.spring.proxy.common.service.ConcreteService;
+import me.chulgil.spring.proxy.common.service.IService;
+import me.chulgil.spring.proxy.common.service.ServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.AopUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+public class ProxyFactoryTest {
+
+    @Test
+    @DisplayName("인터페이스가 있으면 JDK 동적 프록시 사용")
+    public void interfaceProxyTest() throws Exception {
+        //given
+        IService target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvice(new TimeAdvice());
+        IService proxy = (IService) proxyFactory.getProxy();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        //when
+        proxy.save();
+
+        //then
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isTrue();
+        assertThat(AopUtils.isCglibProxy(proxy)).isFalse();
+    }
+
+    @Test
+    @DisplayName("구체클래스가 있으면 CGLIB 사용")
+    public void concreteProxyTest() throws Exception {
+        //given
+        ConcreteService target = new ConcreteService();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvice(new TimeAdvice());
+        ConcreteService proxy = (ConcreteService) proxyFactory.getProxy();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        //when
+        proxy.call();
+
+        //then
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+        assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
+    }
+
+    @Test
+    @DisplayName("인터페이스가 있더라도 CGLIB 사용 강제")
+    public void proxyTargetClassTest() throws Exception {
+        //given
+        IService target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true);
+        proxyFactory.addAdvice(new TimeAdvice());
+
+        IService proxy = (IService) proxyFactory.getProxy();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        //when
+        proxy.save();
+
+        //then
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+        assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
+    }
+}
