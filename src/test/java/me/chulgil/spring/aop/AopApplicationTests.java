e9a42a3857a1f3d27f6731d8e1ae212d8db73457
chulgil <contact@chulgil.me>
2022-04-15 16:05:27 +0900

    Feat: 스프링 AOP 구현1 - 시작

--- src/test/java/me/chulgil/spring/aop/AopApplicationTests.java
+++ src/test/java/me/chulgil/spring/aop/AopApplicationTests.java
@@ -1,7 +1,9 @@
 package me.chulgil.spring.aop;
 
+import me.chulgil.spring.aop.order.aop.AspectV1;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
 class AopApplicationTests {
